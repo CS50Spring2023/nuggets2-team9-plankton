defc264ded0e3e9029a564435d87eab337dc786a
Sanjana Raj <sanjana.raj.25@dartmouth.edu>
2023-05-18 22:28:00 -0400

    load_grid, assign_random_spot, update_grids written

--- grid.c
+++ grid.c
@@ -1,11 +1,9 @@
 /*
- * grid.c     Plankton     May, 2023
- *
- * add high level description
- *
- * refer to the requirement, design, and implementation specs for more information
- *     
- */
+grid.c 
+module for handling the global and player grids
+
+Team 9: Plankton, May 2023
+*/
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -14,7 +12,7 @@
 
 #include "libs/file.h"
 #include "libs/mem.h"
-
+#include "game.h"
 
 // just to deal with executable compilation error, delete later
 int main()
@@ -22,35 +20,10 @@ int main()
     return 1;
 }
 
-// /*
-// * load_grid: takes in a FILE* fp to a map file which is assumed to be valid
-// * Reads the file into an array of strings, each string in the array represents a row of the map
-// *
-// */
-// char**
-// load_grid(FILE* fp)
-// {
-//     if (fp == NULL){
-//         fprintf(stderr, "Error. NULL file pointer passed to load_map.\n");
-//         exit(1);
-//     }
 
-//     // Create an array of strings, each string is one row of the map
-//     char** grid = mem_malloc_assert(file_numLines(fp) * sizeof(char*), "Error allocating memory in load_grid.\n");
-
-//     char* newRow = NULL;
-//     int row = 0; // keeps track of our position while filling in grid array
-
-//     while ((newRow = file_readLine(fp)) != NULL){
-//         grid[row] = newRow;
-//         row++;
-//     }
-
-//     return grid;
-
-// }
 
 /**************** functions  ****************/
+// rewritten for the new grid format: original code is below/commented out
 
 /*
 * load_grid: takes in a FILE* fp to a map file which is assumed to be valid
@@ -93,59 +66,27 @@ char* load_grid(FILE* fp)
     return grid;
 }
 
-// idk if i need this
-// char**
-// empty_player_grid(int rows, int columns)
-// {
-//     char** grid = mem_malloc_assert(rows * sizeof(char*), "Error allocating memory in empty_player_grid.\n");
-
-//     for (int r = 0; r < rows; r++){
-
-//         for(int c = 0; c < columns; c++){
-
-//         }
-//     }
-// }
-
-
-/*
-* grid_toStr: converts a grid to a string that can be sent to and displayed by the client
-* Takes in a char** global_grid and optionally a char** player_grid (which can be null) as well as the number of rows and columns in the grid
-* If a player_grid is NULL the output string will just be the global_grid
-*
-*/
-char*
-grid_toStr(char** global_grid, char** player_grid, int rows, int columns)
-{
-    // Create string for string version of grid map, must have rows*columns characters plus new lines
-   char* display = mem_malloc_assert((row*columns) + rows - 1, "Error allocating memory in grid_toStr.\n");
-
-   for (int r = 0; r < rows; r++){
-        for (int c = 0; c < columns; c++){
-            // adding 1 because null terminator isn't included in column count
-            display[(r* (columns + 1)) + c] = global_grid[r][c];
-           
-            if (player_grid != NULL){
-                // if a player grid was passed in, set to player_grid value
-                display[(r* (columns + 1)) + c] = player_grid[r][c];
-            }
-        }
-        // don't add a new line after the last row
-        if (r < rows - 1){
-            display[(r* (columns + 1)) + columns + 1] = '\n'; // add new line to the end of each row
-        }
-    }
-
-   return display;
-
-}
 
 void
 assign_random_spot(char** grid, int rows, int columns, char thing)
 {
     // assigns a "thing" to a random open spot, can be used to place either gold or a player
+    bool placed = false;
 
+    int max = columns*rows;
+    while (placed == false){
+        // get a pseudo-random number from stdlib, between 0 and the largest index
+        int r = rand() % max;
+        // try to place the "thing" there
+        if (strcmp(grid[r], '.')==0){
+            grid[r]=='*';
+            placed = true;
+        }
+        // try again with another random spot if it didn't work
+    }
 }
+
+
 
 void
 update_player_grid(char** player_grid, char** global_grid, int pr, int pc)
@@ -168,10 +109,14 @@ update_player_grid(char** player_grid, char** global_grid, int pr, int pc)
 }
 
 void
-update_grids(char** global_grid, game struct)
+update_grids(char** global_grid, game_t* game)
 {
-    loops thru all players
-    calls update player grid 
+    char** clients = game->clients;
+    // loops thru all players
+    for (int i=0; i<game->playersJoined; i++){
+         // calls update player grid 
+         update_player_grid();
+    }
 }
 
 
@@ -346,4 +291,81 @@ isVisible(int pr, int pc, int wr, int wc)
     
 //     return invisible
 //     */
+// }
+
+/**************** old functions  ****************/
+
+// /*
+// * load_grid: takes in a FILE* fp to a map file which is assumed to be valid
+// * Reads the file into an array of strings, each string in the array represents a row of the map
+// *
+// */
+// char**
+// load_grid(FILE* fp)
+// {
+//     if (fp == NULL){
+//         fprintf(stderr, "Error. NULL file pointer passed to load_map.\n");
+//         exit(1);
+//     }
+
+//     // Create an array of strings, each string is one row of the map
+//     char** grid = mem_malloc_assert(file_numLines(fp) * sizeof(char*), "Error allocating memory in load_grid.\n");
+
+//     char* newRow = NULL;
+//     int row = 0; // keeps track of our position while filling in grid array
+
+//     while ((newRow = file_readLine(fp)) != NULL){
+//         grid[row] = newRow;
+//         row++;
+//     }
+
+//     return grid;
+
+// }
+
+
+// idk if i need this
+// char**
+// empty_player_grid(int rows, int columns)
+// {
+//     char** grid = mem_malloc_assert(rows * sizeof(char*), "Error allocating memory in empty_player_grid.\n");
+
+//     for (int r = 0; r < rows; r++){
+
+//         for(int c = 0; c < columns; c++){
+
+//         }
+//     }
+// }
+
+/*
+* grid_toStr: converts a grid to a string that can be sent to and displayed by the client
+* Takes in a char** global_grid and optionally a char** player_grid (which can be null) as well as the number of rows and columns in the grid
+* If a player_grid is NULL the output string will just be the global_grid
+*
+*/
+// char*
+// grid_toStr(char** global_grid, char** player_grid, int rows, int columns)
+// {
+//     // Create string for string version of grid map, must have rows*columns characters plus new lines
+//    char* display = mem_malloc_assert((row*columns) + rows - 1, "Error allocating memory in grid_toStr.\n");
+
+//    for (int r = 0; r < rows; r++){
+//         for (int c = 0; c < columns; c++){
+//             // adding 1 because null terminator isn't included in column count
+//             display[(r* (columns + 1)) + c] = global_grid[r][c];
+           
+//             if (player_grid != NULL){
+//                 // if a player grid was passed in, set to player_grid value
+//                 display[(r* (columns + 1)) + c] = player_grid[r][c];
+//             }
+//         }
+//         // don't add a new line after the last row
+//         if (r < rows - 1){
+//             display[(r* (columns + 1)) + columns + 1] = '\n'; // add new line to the end of each row
+//         }
+//     }
+
+//    return display;
+
 // }
