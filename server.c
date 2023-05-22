ce1447ac2d5a9193036107e6285cddab7860bc84
Jackson Easley <jackson.easley@gmail.com>
2023-05-22 13:56:33 -0400

    added code to game and server to handle movements 5/22

--- server.c
+++ server.c
@@ -33,38 +33,71 @@ handle_movement(client_t* player, char key, game_t* game)
 
 
     switch (key) {
-        // update new Pos based on the case
-        case 'h': ... code for letter=='B'; break;
-        case 'l': ... code for letter=='C'; break;
-        case 'j': ... code for letter=='A'; break;
-        case 'k': ... code for letter=='B'; break;
-        case 'y': ... code for letter=='C'; break;
-        case 'u': ... code for letter=='A'; break;
-        case 'b': ... code for letter=='B'; break;
-        case 'n': ... code for letter=='C'; break;
-        default:  ... code for letter not matching any case above.
+        // update new Pos based on the key inputted
+        case 'h': newPos_x--; break;
+        case 'l': newPos_x++; break;
+        case 'j': newPos_y++; break;
+        case 'k': newPos_y--; break;
+        case 'y': newPos_x--; newPos_y--; break;
+        case 'u': newPos_x++; newPos_y--; break;
+        case 'b': newPos_x--; newPos_y++; break;
+        case 'n': newPos_x++; newPos_y++; break;
+        default: exit(1);
 
     }
 
     char grid_val = get_grid_value(newPos_x, newPos_y);
+
     if (grid_val == '+' || grid_val == '-' || grid_val == '|' || grid_val == ' '){
         return;
     }
     else if (grid_val == '.' || grid_val == '#'){
+        //change the global grid on the spot they came from back to what it was
+        if (player->onTunnel){
+            change_spot(game, player->x, player->y, '#');
+        }
+        else{
+            change_spot(game, player->x, player->y, '.');
+        }
 
+        player->onTunnel = (grid_val == '#');
+        
+        // change the global grid on the spot they are now on to be their letter
+        change_spot(game, newPos_x, newPos_y, player->id);
+
+        // update the player's position in the player struct
+        update_position(player, newPos_x, newPos_y);
+        
     }
     else if (isalpha(grid_val)){
-        //
+        // find the player there using a game function
+        player_t* other_player = find_player(grid_val);
+
+        // switch the positions of the two players
+        update_position(other_player, player->x, player->y);
+        update_position(player, newPos_x, newPos_y);
+
+        // update the player's records on the spot they stand on 
+        bool player_OnTunnel = player->onTunnel;
+        player->onTunnel = other_player->onTunnel;
+        other_player->onTunnel = player_OnTunnel;
+
+        // update the global grid to reflect change
+        change_spot(game, player->x, player->y, player->id);
+        change_spot(game, other_player->x, other_player->y, other_player->id);
+
+
     }
     else if (grid_val == '*'){
         update_gold(game, player, newPos_x, newPos_y, goldMaxPiles);
-        // send notify message
-        
+        // send gold notify message
+        //change the global grid on the spot they came from back to what it was
+        // change the global grid on the spot they are now on to be their letter
+        update_position(player, newPos_x, newPos_y);
     }
-
-
-     
    
 }
 
 // make spectator quit
+
+// gold notification
