5558d452db9e94f13788fa63076e7f6611c4d4dc
Andra Pantelimon <ioana.andrada.pantelimon.th@dartmouth.edu>
2023-05-30 10:55:11 -0400

    grid header

--- common/grid.h
+++ common/grid.h
@@ -26,7 +26,8 @@
 
 /*
  * load_grid
- * Loads the game grid from a file.
+ * Loads the game grid from a file. 
+ * Specifically, reads a file into an array of strings, with each string in the array representing a row of the game map.
  * Inputs:
  *   - fp: Pointer to the file containing the grid.
  *   - rows: Pointer to the variable that will store the number of rows in the grid.
@@ -39,6 +40,7 @@ char** load_grid(FILE* fp, int* rows, int* columns);
 /*
  * load_player_grid
  * Loads the player's grid based on the game state.
+ * The player's grid starts off empty and has an equal rows and columns as the global grid.
  * Inputs:
  *   - game: Pointer to the game state structure.
  * Outputs:
@@ -61,7 +63,8 @@ char* grid_toStr(char** global_grid, char** player_grid, int rows, int columns);
 
 /*
  * assign_random_spot
- * Assigns a random spot in the grid for a given thing.
+ * Assigns a random spot in the grid for a given object.
+ * Used to randomly place players and gold.
  * Inputs:
  *   - grid: Pointer to the game grid array.
  *   - rows: Number of rows in the grid.
@@ -75,7 +78,7 @@ void assign_random_spot(char** grid, int rows, int columns, char thing, int* spo
 
 /*
  * get_grid_value
- * Retrieves the value at a specific position in the game grid.
+ * Retrieves the value/symbol at a specific position in the game grid.
  * Inputs:
  *   - game: Pointer to the game state structure.
  *   - r: Row index in the grid.
@@ -87,7 +90,7 @@ char get_grid_value(game_t* game, int r, int c);
 
 /*
  * change_spot
- * Changes the value at a specific position in the game grid.
+ * Changes the value/symbol at a specific position in the game grid.
  * Inputs:
  *   - game: Pointer to the game state structure.
  *   - r: Row index in the grid.
@@ -99,7 +102,8 @@ void change_spot(game_t* game, int r, int c, char symbol);
 
 /*
  * is_visible
- * Checks if a position in the grid is visible to the player.
+ * Checks if a position in the grid is visible from the player's current location.
+ * Checks spots on each row and column between the player and the position investigated for boundaries that might obstruct visibility.
  * Inputs:
  *   - game: Pointer to the game state structure.
  *   - playerColumn: Column index of the player's position.
@@ -114,6 +118,8 @@ bool is_visible(game_t* game, const int playerColumn, const int playerRow, const
 /*
  * get_player_visible
  * Retrieves the visibility status of a player.
+ * Loops over each point in the grid and calls `is_visible`, updating the player's grid accordingly. 
+ * Returns true if the player's grid was modified.
  * Inputs:
  *   - game: Pointer to the game state structure.
  *   - player: Pointer to the player structure.
